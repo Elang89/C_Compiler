d22a87b676a5fa29c5e4aebb8b4085792b01a328
elang8899 <elang8899@gmail.com>
2016-02-13 15:52:09 -0600

    commit inicial

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+
+	int b_file_path;
+	char file_path[1024];
+	printf("Specify a valid file path to compile file: ");
+	
+	fgets(file_path, sizeof(file_path), stdin);
+	file_path[ strcspn(file_path, "\n") ] = '\0';
+	b_file_path = validate_file_path(file_path);
+
+	while(b_file_path == 0){
+		printf("Invalid file path, please specify a valid file path: ");
+		fgets(file_path, sizeof(file_path), stdin);
+		file_path[ strcspn(file_path, "\n") ] = '\0';
+		b_file_path = validate_file_path(file_path);
+	}
+	printf("File found, generating output... \n");
+	return 0;
+}
+
+int validate_file_path(const char* path){
+	if(access(path, F_OK)!= -1){
+		return 1;
+	}
+	return 0;
+}
