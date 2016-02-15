3e2045441e7ef0de11165cb8e4c40a9d5e9062fe
emmanuelrs <emmanuelrs@gmail.com>
2016-02-14 20:39:14 -0600

    scanner

--- scanner.h
+++ scanner.h
@@ -3,20 +3,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 
 void scanner(char * file_path)
 {
 	FILE * file;
-	char input_character;
+	int input_character, c;
 	file = fopen(file_path, "r");
 
 	while((input_character = fgetc(file)) != EOF)
 	{
-		if(input_character == 'b')
+		if (isspace(input_character))
 		{
-			putchar(input_character + "\n");
+			continue;
+
 		}
+		else if (isalpha(input_character))
+		{
+			buffer_char(input_character);
+			for(c = getchar(); isalmun(c) || c == '_'; c = getchar())
+			{
+				buffer_char(c);
+			}
+			ungetc(c, stdin);
+			return check_reserved();
+		} 
+
 	}	
 
 }
