ee08cd2c5dc30c5102b3ea1f8b16d48f922275be
Elang89 <elang8899@gmail.com>
2016-03-06 19:11:30 -0600

    ejemplo

--- testMicro.asm
+++ testMicro.asm
@@ -5,8 +5,9 @@
 	  .text
 main: 
 
-something: .word 10 
+var: .word 3 
 hola: .word 6 
+h: .word 2 
 li $t0, 2 
 li $v0, 1 
 add $a0, $t0, $zero 
@@ -15,16 +16,7 @@ li $t0, 4
 li $v0, 1 
 add $a0, $t0, $zero 
 syscall 
-li $t0, 20 
-li $v0, 1 
-add $a0, $t0, $zero 
-syscall 
-li $t0, 40 
-li $v0, 1 
-add $a0, $t0, $zero 
-syscall 
-h: .word 2 
-lw $t1,something 
+lw $t1,var 
 lw $t1,hola 
 li $v0, 10 
 syscall 
